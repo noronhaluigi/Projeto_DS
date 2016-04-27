597e46bb3ccabac9e50c69e7a5cc0ab5f17cef28
Luigi Noronha Lima de Almeida <Luigi Noronha Lima de Almeida>
2016-04-27 03:44:41 -0300

    Após analisar bem o código enviado pelo Felipe, tive que reestruturar o meu código e finalizar as implementações. Para isso, primeiramente tentei refazer o codigo com a ideia de matrizes, o que não deu certo devido a quantidade de erros no meu programa. Decidi voltar para o método antigo, botão por botão. Modifiquei os commands de cada botão a partir de uma função que impediria o roubo do outro jogador, e também aplicando a marca do jogador (O ou X) dependendo da vez. Também apliquei uma função de nome Resultados em que há a mudança do label para cada jogada do jogador, registrando-as, e também registrando as vitórias dos jogadores O ou X. No entanto, essa função não está funcionando corretamente. Tentarei corrigir esse erro brevemente. Também modifiquei a função reinicia do tabuleiro, fazendo com que os botões fossem apenas alterados sem ter que montar e substituir novos botões, podendo causar eventuais problemas na programação. No momento, concentro esforços para resolver o bug da função Resultados.

--- Classe_Tabulero.py
+++ Classe_Tabulero.py
@@ -7,8 +7,14 @@ Created on Mon Apr 25 18:28:25 2016
 
 import tkinter as tk
 from Ep3 import Jogo_da_Velha
+
+
+import tkinter.messagebox as tkm
 class Tabulero:
     def __init__(self):
+        
+        self.Velha = Jogo_da_Velha()        
+        
         self.window = tk.Tk()
         self.window.title("Jogo da Velha!")        
         self.window.geometry("350x370")
@@ -18,83 +24,191 @@ class Tabulero:
         self.window.columnconfigure(0, minsize=100, weight=1)
         self.window.columnconfigure(1, minsize=100, weight=1)
         self.window.columnconfigure(2, minsize=100, weight=1)
-        
-        x = Jogo_da_Velha()
         self.window.rowconfigure(3, minsize=20, weight=1)
-        texto_jogada = tk.Label(self.window)
-        texto_jogada.configure(text= x.recebe_jogada())
-        texto_jogada.configure(font="Courier 20 bold")     
-        texto_jogada.grid(row=3,column=0, columnspan=3,sticky = "nsew" )
+       
+        self.jogada = tk.Label(self.window, width= 50, text = "Proximo jogador: X", anchor = "w")
+        self.jogada.grid(row=3, column=0, columnspan=3)
         
-         
 
 
-        botão1 = tk.Button(self.window)
-        botão1.configure(text=" ")
-        botão1.configure(command=self.jogador)
-        botão1.grid(row=0, column=0, sticky="nsew")
+        self.botão1 = tk.Button(self.window)
+        self.botão1.configure(text=" ")
+        self.botão1.configure(command=self.Caso_B1)
+        self.botão1.grid(row=0, column=0, sticky="nsew")
 
-        botão2 = tk.Button(self.window)
-        botão2.configure(text=" ")
-        botão2.configure(command=self.jogador)
-        botão2.grid(row=0, column=1, sticky="nsew")
+        self.botão2 = tk.Button(self.window)
+        self.botão2.configure(text=" ")
+        self.botão2.configure(command=self.Caso_B2)
+        self.botão2.grid(row=0, column=1, sticky="nsew")
+
+        self.botão3 = tk.Button(self.window)
+        self.botão3.configure(text=" ")
+        self.botão3.configure(command=self.Caso_B3)
+        self.botão3.grid(row=0, column=2, sticky="nsew")
+        
+        self.botão4 = tk.Button(self.window)
+        self.botão4.configure(text=" ")
+        self.botão4.configure(command=self.Caso_B4)
+        self.botão4.grid(row=1, column=0, sticky="nsew")
+        
+        self.botão5 = tk.Button(self.window)
+        self.botão5.configure(text=" ")
+        self.botão5.configure(command=self.Caso_B5)
+        self.botão5.grid(row=1, column=1, sticky="nsew")
+        
+        self.botão6 = tk.Button(self.window)
+        self.botão6.configure(text=" ")
+        self.botão6.configure(command=self.Caso_B6)
+        self.botão6.grid(row=1, column=2, sticky="nsew")
+
+        self.botão7 = tk.Button(self.window)
+        self.botão7.configure(text=" ")
+        self.botão7.configure(command=self.Caso_B7)
+        self.botão7.grid(row=2, column=0, sticky="nsew")
+        
+        self.botão8 = tk.Button(self.window)
+        self.botão8.configure(text=" ")
+        self.botão8.configure(command=self.Caso_B8)
+        self.botão8.grid(row=2, column=1, sticky="nsew")
+        
+        self.botão9 = tk.Button(self.window)
+        self.botão9.configure(text=" ")
+        self.botão9.configure(command=self.Caso_B9)
+        self.botão9.grid(row=2, column=2, sticky="nsew")
+
+    def Resultados(self):
+        if self.jogada["text"] == "Proximo Jogador: X":
+            self.jogada["text"] = "Proximo Jogador: O"
+        
+        if self.jogada["text"] == "Proximo Jogador: O":
+            self.jogada["text"] = "Proximo Jogador: X"
+                
+        if self.Velha.verifica_ganhador()==1:
+            tkm.showinfo(message = "Parabéns jogador X! Você ganhou!")
+            self.jogada["text"]="Proximo Jogador: X"
+            self.reinicia()
+            self.Velha.jogador = "O"
+            self.Velha.limpa_jogadas()
+            
+        if self.Velha.verifica_ganhador()==2:
+            tkm.showinfo(message = "Parabéns jogador O! Você ganhou!")
+            self.jogada["text"]="Proximo Jogador: O"
+            self.reinicia()
+            self.Velha.jogador = "X"
+            self.Velha.limpa_jogadas()
+            
+        if self.Velha.verifica_ganhador()==0:
+            tkm.showinfo(message = "Deu velha!")
+            self.jogada["text"]="Proximo Jogador: X"
+            self.reinicia()
+            self.Velha.jogador = "O"
+            self.Velha.limpa_jogadas()
+              
+    def B_1_Clickado(self):
+        self.Velha.recebe_jogada(0,0)
+        self.botão1.configure(text = self.Velha.jogador, state = "disabled") 
+        self.Velha.verifica_ganhador()
+            
+    def B_2_Clickado(self):
+        self.Velha.recebe_jogada(0,1)
+        self.botão2.configure(text = self.Velha.jogador, state = "disabled")
+        self.Velha.verifica_ganhador()
+        
+    def B_3_Clickado(self):
+        self.Velha.recebe_jogada(0,2)
+        self.botão3.configure(text = self.Velha.jogador, state = "disabled") 
+        self.Velha.verifica_ganhador()
+        
+    def B_4_Clickado(self):
+        self.Velha.recebe_jogada(1,0)
+        self.botão4.configure(text = self.Velha.jogador, state = "disabled")    
+        self.Velha.verifica_ganhador()
+        
+    def B_5_Clickado(self):
+        self.Velha.recebe_jogada(1,1)
+        self.botão5.configure(text = self.Velha.jogador, state = "disabled")   
+        self.Velha.verifica_ganhador()
+        
+    def B_6_Clickado(self):
+        self.Velha.recebe_jogada(1,2)
+        self.botão6.configure(text = self.Velha.jogador, state = "disabled")    
+        self.Velha.verifica_ganhador()
+        
+    def B_7_Clickado(self):
+        self.Velha.recebe_jogada(2,0)
+        self.botão7.configure(text = self.Velha.jogador, state = "disabled") 
+        self.Velha.verifica_ganhador()
+        
+    def B_8_Clickado(self):
+        self.Velha.recebe_jogada(2,1)
+        self.botão8.configure(text = self.Velha.jogador, state = "disabled")
+        self.Velha.verifica_ganhador()
+        
+    def B_9_Clickado(self):
+        self.Velha.recebe_jogada(2,2)
+        self.botão9.configure(text = self.Velha.jogador, state = "disabled")
+        self.Velha.verifica_ganhador() 
+    
+    def Caso_B1(self):
+        self.B_1_Clickado()
+        return self.Resultados()    
+    def Caso_B2(self):
+        self.B_2_Clickado()
+        return self.Resultados()
+    def Caso_B3(self):
+        self.B_3_Clickado()
+        return self.Resultados()
+    def Caso_B4(self):
+        self.B_4_Clickado()
+        return self.Resultados()
+    def Caso_B5(self):
+        self.B_5_Clickado()
+        return self.Resultados()    
+    def Caso_B6(self):
+        self.B_6_Clickado()
+        return self.Resultados()
+    def Caso_B7(self):
+        self.B_7_Clickado()
+        return self.Resultados()
+    def Caso_B8(self):
+        self.B_8_Clickado()
+        return self.Resultados()
+    def Caso_B9(self):
+        self.B_9_Clickado()
+        return self.Resultados()
 
 
-        botão3 = tk.Button(self.window)
-        botão3.configure(text=" ")
-        botão3.configure(command=self.jogador)
-        botão3.grid(row=0, column=2, sticky="nsew")
-        
-        botão4 = tk.Button(self.window)
-        botão4.configure(text=" ")
-        botão4.configure(command=self.jogador)
-        botão4.grid(row=1, column=0, sticky="nsew")
-        
-        botão5 = tk.Button(self.window)
-        botão5.configure(text=" ")
-        botão5.configure(command=self.jogador)
-        botão5.grid(row=1, column=1, sticky="nsew")
-        
-        botão6 = tk.Button(self.window)
-        botão6.configure(text=" ")
-        botão6.configure(command=self.jogador)
-        botão6.grid(row=1, column=2, sticky="nsew")
 
-        botão7 = tk.Button(self.window)
-        botão7.configure(text=" ")
-        botão7.configure(command=self.jogador)
-        botão7.grid(row=2, column=0, sticky="nsew")
-        
-        botão8 = tk.Button(self.window)
-        botão8.configure(text=" ")
-        botão8.configure(command=self.jogador)
-        botão8.grid(row=2, column=1, sticky="nsew")
-        
-        botão9 = tk.Button(self.window)
-        botão9.configure(text=" ")
-        botão9.configure(command=self.jogador)
-        botão9.grid(row=2, column=2, sticky="nsew")
+
+
+
 
     def iniciar(self):
         self.window.mainloop()
 
-    def click_botao(self, botao) :
-        print("O botão {0} foi clickado").format(botao)
-        botao.configure(text=self.jogador)
+
         
     def reinicia(self):
-        botão1.configure(text = "", state="normal")
-        botão2.configure(text = "", state="normal")
-        botão3.configure(text = "", state="normal")
-        botão4.configure(text = "", state="normal")
-        botão5.configure(text = "", state="normal")
-        botão6.configure(text = "", state="normal")
-        botão7.configure(text = "", state="normal")
-        botão8.configure(text = "", state="normal")
-        botão9.configure(text = "", state="normal") 
+        self.botão1.configure(text = "", state="normal")
+
+        self.botão2.configure(text = "", state="normal")
+
+        self.botão3.configure(text = "", state="normal")
+
+        self.botão4.configure(text = "", state="normal")
+
+        self.botão5.configure(text = "", state="normal")
+
+        self.botão6.configure(text = "", state="normal")
+
+        self.botão7.configure(text = "", state="normal")
+
+        self.botão8.configure(text = "", state="normal")
+
+        self.botão9.configure(text = "", state="normal") 
 
                 
 
 
-jogo = Tabulero()
-jogo.iniciar()
+Velha = Tabulero()
+Velha.iniciar()
